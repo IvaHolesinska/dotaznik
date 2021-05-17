22be96145388a8055bd02c21d750d0290da00bb0
Iva Holešinská <holesinska.iva@gmail.com>
2021-05-17 20:43:31 +0200

    změna smajlíka

--- src/Question/index.jsx
+++ src/Question/index.jsx
@@ -1,22 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import QuestionBody from '../QuestionBody/index';
 import Option from '../Option/index';
 import './style.css';
 
-const Question = (props) => (
-  <div className="question">
-    <QuestionBody 
-      iconType="symbolQuestion" 
-      text="Kdo jinému jámu kopá, sám do ní padá"
-    />
-    <div className="question__options">
-      <Option type="smileyStrongYes" text="Souhlasím"/>
-      <Option type="smileyYes" text="Spíše souhlasím"/>
-      <Option type="smileyNeutral" text="Nevím"/>
-      <Option type="smileyNo" text="Spíše nesouhlasím"/>
-      <Option type="smileyStrongNo" text="Nesouhlasím"/>
+const Question = () => {
+  // const [answered, setAnswered] = useState(false);
+  const [answer, setAnswer] = useState('symbolQuestion');
+
+  const handleSelect = (iconType) => {
+    return setAnswer(iconType);
+  };
+
+  // const handleSelect = () => {
+  //   return setAnswered(true);
+  // };
+
+  return (
+    <div className="question">
+      <QuestionBody
+        iconType={answer}
+        text="Kdo jinému jámu kopá, sám do ní padá"
+      />
+      <div className="question__options">
+        <Option
+          type="smileyStrongYes"
+          text="Souhlasím"
+          onSelected={handleSelect}
+        />
+        <Option
+          type="smileyYes"
+          text="Spíše souhlasím"
+          onSelected={handleSelect}
+        />
+        <Option type="smileyNeutral" text="Nevím" onSelected={handleSelect} />
+        <Option
+          type="smileyNo"
+          text="Spíše nesouhlasím"
+          onSelected={handleSelect}
+        />
+        <Option
+          type="smileyStrongNo"
+          text="Nesouhlasím"
+          onSelected={handleSelect}
+        />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Question;
